ed98f8205fca26e56a35c1e6a172a2b94d258694
Ryan <saltburn6969@gmail.com>
2025-12-11 11:31:16 +0530

    c

--- pages/api/og.js
+++ pages/api/og.js
@@ -1,19 +1,8 @@
-import { PikaSdk } from "pika-sdk";
-const pikaSdk = new PikaSdk(process.env.PIKA_API_KEY);
+
 
 export default async function handler(req, res) {
   const { title, description } = req.query;
 
-  // const response = await pikaSdk.generateImageFromTemplate(
-  //   "open-graph-image-1",
-  //   {
-  //     title: title,
-  //     description: description,
-  //     textColor: "#fff",
-  //     backgroundImageUrl: "https://rishimohan.me/images/site/meta-bg.png",
-  //   },
-  //   "binary"
-  // );
 
   const response = await fetch("https://api.orshot.com/v1/generate/images", {
     method: "POST",
